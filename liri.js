daaa237b2d6762a594e66091e3a291eaa6e3fec3
Shruti Gaonkar <shruti.korgaonkar@gmail.com>
2019-12-17 23:12:24 -0800

    Added spotify functionality to get song details

--- liri.js
+++ liri.js
@@ -5,8 +5,7 @@ var keys = require("./js/keys");
 var inquirer = require("inquirer");
 var axios = require("axios");
 var moment = require('moment');
-
-var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
 
 inquirer.prompt([
 
@@ -18,6 +17,7 @@ inquirer.prompt([
     }
 ]).then(function (respCat) {
     var searchCat = respCat.category;
+
     inquirer.prompt([
         {
             type: "input",
@@ -39,6 +39,7 @@ inquirer.prompt([
             }
         }
     ])
+    //search = null;
     /*.then(function (response) {
         var searchTerm = response.term;
         if (searchCat == "Band") {
@@ -52,52 +53,49 @@ function Search(keyword) {
     this.keyword = keyword;
     this.getBand = function () {
         var url = "https://rest.bandsintown.com/artists/" + this.keyword + "/events?app_id=codingbootcamp";
-        axios.get(url).then(
-            function (response) {
-                var bandArr = response.data;
-                for (var i = 0; i < bandArr.length; i++) {
-                    var eventDate = moment(bandArr[i].datetime).format('MM/DD/YYYY');
-                    console.log(`Name of the venue: ${bandArr[i].venue.name}
+        axios.get(url).then(function (response) {
+            var bandArr = response.data;
+            for (var i = 0; i < bandArr.length; i++) {
+                var eventDate = moment(bandArr[i].datetime).format('MM/DD/YYYY');
+                console.log(`Name of the venue: ${bandArr[i].venue.name}
 Venue location: ${bandArr[i].venue.city}, ${bandArr[i].venue.country}
 Date of the event: ${eventDate}
  ---------------------------------------`);
-                }
-            })
-            .catch(function (error) {
-                if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
-                    //console.log("---------------Data---------------");
-                    if (error.response.data.errorMessage.includes('[NotFound]')) {
-                        console.log("\nThe artist was not found");
-                    } else {
-                        console.log(error.response.data);
-                    }
-                    //console.log(error.response.data);
-                    //console.log("---------------Status---------------");
-                    //console.log(error.response.status);
-                    //console.log("---------------Status---------------");
-                    //console.log(error.response.headers);
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                    console.log(error.request);
+            }
+        }).catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                //console.log("---------------Data---------------");
+                if (error.response.data.errorMessage.includes('[NotFound]')) {
+                    console.log("\nThe artist was not found");
                 } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log("Error", error.message);
+                    console.log(error.response.data);
                 }
-                //console.log(error.config);
-            });
-
-    },
-        this.getMovie = function () {
-            this.keyword = (this.keyword) ? this.keyword : "Mr. Nobody";
-            var url = "https://www.omdbapi.com/?t=" + this.keyword + "&y=&plot=short&apikey=trilogy";
-            axios.get(url).then(
-                function (response) {
-                    var movieResp = response.data;
-                    //console.log(movieArr);
-                    console.log(`
+                //console.log(error.response.data);
+                //console.log("---------------Status---------------");
+                //console.log(error.response.status);
+                //console.log("---------------Status---------------");
+                //console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            //console.log(error.config);
+        });
+    };
+    this.getMovie = function () {
+        this.keyword = (this.keyword) ? this.keyword : "Mr. Nobody";
+        var url = "https://www.omdbapi.com/?t=" + this.keyword + "&y=&plot=short&apikey=trilogy";
+        axios.get(url).then(
+            function (response) {
+                var movieResp = response.data;
+                //console.log(movieArr);
+                console.log(`
 -----------------------------------------------------------------------------------------------                    
 Title of the movie: ${movieResp.Title}
 Year the movie came out: ${movieResp.Year}
@@ -108,11 +106,36 @@ Language of the movie: ${movieResp.Language}
 Plot of the movie: ${movieResp.Plot}
 Actors in the movie: ${movieResp.Actors}
 -----------------------------------------------------------------------------------------------`);
-                })
-                .catch(function (error) {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log("\nThere was an error processing the request");
-                });
+            }).catch(function (error) {
+                // Something happened in setting up the request that triggered an Error
+                console.log("\nThere was an error processing the request");
+            });
+    };
 
-        }
-}
+    this.getSong = function () {
+        var spotify = new Spotify(keys.spotify);
+        this.keyword = (this.keyword) ? this.keyword : "The Sign,Ace of Base";
+        spotify.search({ type: 'track,artist', query: this.keyword, limit: 10 }).then(function (response) {
+            //console.log(response.tracks.items);
+            let tracksResp = response.tracks.items;
+            //console.log(tracksResp[0].artists);
+            for (var i = 0; i < tracksResp.length; i++) {
+                let artistsResp = tracksResp[i].artists;
+                console.log("\nArtists:");
+                for (var j = 0; j < artistsResp.length; j++) {
+                    console.log(`${artistsResp[j].name}`);
+                }
+                console.log(`\nSong: ${tracksResp[i].name}
+Preview link: ${(tracksResp[i].preview_url) ? tracksResp[i].preview_url : '-'}
+Album: ${tracksResp[i].album.name}
+------------------------------------------------------------------------`);
+
+            }
+        }).catch(function (err) {
+            console.log(err);
+        });
+    };
+
+
+};
+
